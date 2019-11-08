4cde68a63e650c854a9a6a5e3d38c0e1a7384a42
nanlong <200006506@qq.com>
2019-11-08 15:53:52 +0800

    add trend macd

---
+++ apps/trend_tracker/lib/trend_tracker/trend/macd.ex
@@ -0,0 +1,44 @@
+defmodule TrendTracker.Trend.Macd do
+  @moduledoc """
+  MACD - 异同移动平均线
+  """
+  use TrendTracker.System
+
+  def default do
+    [fast: 12, slow: 26, signal: 9]
+  end
+
+  def indicators(state) do
+    fast = get_params(state, :fast)
+    slow = get_params(state, :slow)
+    signal = get_params(state, :signal)
+    ema_fast = "ema_#{fast}"
+    ema_slow = "ema_#{slow}"
+
+    [
+      [{:ema, "close", fast}, rename: ema_fast],
+      [{:ema, "close", slow}, rename: ema_slow],
+      {:dif, ema_fast, ema_slow},
+      [{:ema, "dif", signal}, rename: "dea"],
+      {:hist, 1},
+    ]
+  end
+
+  def klines(state) do
+    state[:klines]
+    |> Enum.slice(-2, 2)
+    |> Enum.map(fn kline ->
+      Map.take(kline, ["id", "datetime", "update_at", "open", "close", "high", "low", "dif", "dea", "hist"])
+    end)
+  end
+
+  def handle_call(:trend, _from, state) do
+    [pre_kline, cur_kline] = Enum.slice(state[:klines], -2, 2)
+
+    cond do
+      cur_kline["hist"] > pre_kline["hist"] && cur_kline["dif"] > cur_kline["dea"] -> :long
+      cur_kline["hist"] < pre_kline["hist"] && cur_kline["dif"] < cur_kline["dea"] -> :short
+      true -> nil
+    end
+  end
+end
