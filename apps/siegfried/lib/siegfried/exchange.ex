ec996204b57c86c449cf82fb7f867ef71f31931b
nanlong <200006506@qq.com>
2019-11-14 16:08:25 +0800

    update Exchange.list_klines/5

--- apps/siegfried/lib/siegfried/exchange.ex
+++ apps/siegfried/lib/siegfried/exchange.ex
@@ -8,17 +8,39 @@ defmodule Siegfried.Exchange do
 
   alias Siegfried.Exchange.Kline
 
-  def list_klines(exchange, symbol, period) do
-    Kline
-    |> where([k], k.exchange == ^exchange)
-    |> where([k], k.symbol == ^symbol)
-    |> where([k], k.period == ^period)
-    |> order_by(asc: :timestamp)
-    |> Repo.all()
+  def list_klines(exchange, symbol, period, from \\ nil, to \\ nil) do
+    from = from || 0
+    to = to || :os.system_time(:second)
+
+    query = from k in Kline,
+      where: k.exchange == ^exchange,
+      where: k.symbol == ^symbol,
+      where: k.period == ^period,
+      where: k.timestamp >= ^from and k.timestamp <= ^to,
+      order_by: [asc: k.timestamp]
+
+    klines = Repo.all(query)
+    kline = List.first(klines)
+
+    # 合约K线可能比较少，使用现货的K线数据
+    if exchange == "huobi" and String.contains?(symbol, "_") and not is_nil(kline) and kline.timestamp > from do
+      kline = List.first(klines)
+      currency = symbol |> String.split("_") |> List.first() |> String.downcase()
+      symbol = "#{currency}usdt"
+      spot_klines = list_klines(exchange, symbol, period, from, kline.timestamp)
+      spot_klines ++ Enum.slice(klines, 1..-1)
+    else
+      klines
+    end
   end
 
   def create_kline(attrs \\ %{}) do
-    kline = Repo.get_by(Kline, exchange: attrs["exchange"], symbol: attrs["symbol"], period: attrs["period"], timestamp: attrs["timestamp"])
+    kline = Repo.get_by(Kline,
+      exchange: attrs["exchange"],
+      symbol: attrs["symbol"],
+      period: attrs["period"],
+      timestamp: attrs["timestamp"]
+    )
 
     case kline do
       nil -> Kline.changeset(%Kline{}, attrs)
